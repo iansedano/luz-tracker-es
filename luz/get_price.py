c757c7398c1c27dfcfb80d0680e80ad03178f174
Ian <iansedano@gmail.com>
2022-07-24 18:38:47 +0200

    clean output

--- luz/get_price.py
+++ luz/get_price.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 from pprint import pp
 
+from functools import reduce
 
 def get_timestamp(date, hour):
     day, month, year = date.split("-")
@@ -12,7 +13,6 @@ def get_timestamp(date, hour):
 
     dt = datetime(year=int(year), month=int(month), day=int(day), hour=int(hour))
     return int(dt.timestamp())
-
 
 # Connect to local database
 engine, connection, metadata = connect_to_luz()
@@ -23,6 +23,11 @@ response = requests.get("https://api.preciodelaluz.org/v1/prices/all?zone=PCB")
 
 price_json = response.json()
 
+def reducer(acc: list, keyval):
+    acc.append((keyval[0], keyval[1]["price"]))
+    return acc
+    
+pp(reduce(reducer, price_json.items(), []))
 
 prices = [
         {"date": get_timestamp(obj["date"], obj["hour"]), "price": obj["price"]}
